64133aad75f4ddc2e0b58da045ce0117baac758c
Bert Witzel <bwitzel82@gmail.com>
2021-04-01 13:30:41 -0400

    update html, css, and js
    style input fields

--- js/script.js
+++ js/script.js
@@ -5,15 +5,18 @@ FSJS Project 2 - Data Pagination and Filtering
 
 "use strict";
 
+// global variables
+const linkList = document.querySelector('.link-list');
+const studentList = document.querySelector('.student-list');
+
 /*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+`showPage` function
+This function creates and inserts the elements needed to display a "page" of nine students
 */
 
 function showPage(list, page) {
    const startIndex = (page * 9) - 9;
    const endIndex = page * 9;
-   const studentList = document.querySelector('.student-list');
    studentList.innerHTML = '';
    for (let i = 0; i < list.length; i++) {
       if ( i >= startIndex && i < endIndex) {
@@ -35,13 +38,12 @@ function showPage(list, page) {
 }
 
 /*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+`addPagination` function
+This function creates and inserts elements for pagination buttons
 */
 
 function addPagination(list) {
    const paginationButtons = Math.ceil(list.length / 9);
-   const linkList = document.querySelector('.link-list');
    linkList.innerHTML = '';
    let count = 1;
    for (let i = 0; i < paginationButtons; i++) {
@@ -69,7 +71,6 @@ function addPagination(list) {
 }
 
 // Call functions
-
 showPage(data, 1);
 addPagination(data);
 
@@ -77,27 +78,27 @@ addPagination(data);
 Create Search Form
 */
 
-const studentsH2 = document.querySelector('.header h2');
-const searchForm = `
-   <label for="search" class="student-search">
-      <span>Search by name</span>
-      <input id="search" placeholder="Search by name...">
-      <button type="button">
-         <img src="img/icn-search.svg" alt="Search icon">
-      </button>
-   </label>
-`;
-studentsH2.insertAdjacentHTML('afterend', searchForm);
+function createSearch() {
+   const studentsH2 = document.querySelector('.header h2');
+   const searchForm = `
+      <label for="search" class="student-search">
+         <span>Search by name</span>
+         <input id="search" placeholder="Search by name...">
+         <button type="button">
+            <img src="img/icn-search.svg" alt="Search icon">
+         </button>
+      </label>
+   `;
+   studentsH2.insertAdjacentHTML('afterend', searchForm);
+}
+createSearch();
 
 /*
 * `searchFilter` function. As user types in search input student cards that match are displayed 
 */
 
-const input = document.getElementById('search');
-
 function searchFilter(list) {
    const searchName = input.value.toUpperCase();
-   console.log(searchName);
    const studentArray = [];
    for ( let i = 0; i < list.length; i++) {
       const studentFirst = list[i].name.first;
@@ -107,13 +108,33 @@ function searchFilter(list) {
          studentArray.push(list[i]);
       } 
    }
-   showPage(studentArray, 1);
-   addPagination(studentArray);
+   console.log(studentArray);
+   if (studentArray.length === 0) {
+      const message = `<h3>I'm sorry, no students match that name.</h3>`;
+      studentList.innerHTML = message;
+      linkList.style.display = 'none';
+   } else {
+      showPage(studentArray, 1);
+      addPagination(studentArray);
+      linkList.style.display = 'initial';
+   }
 }
 
-// addEventListeners for search functionality
+// addEventListeners for search functionality and input styling
+const input = document.getElementById('search');
+const btn = document.querySelector('.student-search button');
 
-input.addEventListener('keyup', () => searchFilter(data));
+input.addEventListener('keyup', () => {
+   searchFilter(data);
+});
+input.addEventListener('focus', () => {
+   btn.style.backgroundColor = 'red';
+   btn.style.border = '1px solid red';
+});
+input.addEventListener('blur', () => {
+   btn.style.backgroundColor = '#1e90ff';
+   btn.style.border = '1px solid #1e90ff';
+});
 input.addEventListener('click', (e) => {
    searchFilter(data);
    e.preventDefault();
